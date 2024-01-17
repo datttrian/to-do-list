06131cc37940965bc9a5ef9a7f692bf9b1931d2a
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-16 21:00:43 -0500

    Login View Login Form

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
             HeaderView()
+            
             // Login Form
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action: {
+                    // Attempt log in
+                }, label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        
+                        Text("Log In")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                })
+            }
             
             // Create Account
             Spacer()
