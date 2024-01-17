c7e6acd2116fbdc810d5be86a38f06a0bba9a593
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-16 20:57:04 -0500

    Login View Header

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Header
+            HeaderView()
+            // Login Form
+            
+            // Create Account
+            Spacer()
+        }
     }
 }
 
--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            LoginView()
         }
-        .padding()
     }
 }
 
