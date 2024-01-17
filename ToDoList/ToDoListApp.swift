276133dbfa512b2df3c0fc1f9693cc63805087ac
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-17 01:10:42 +0000

    Initialize Xcode project for To Do List App

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by datttrian on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
