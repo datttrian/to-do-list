cb96a1b53a5a07d5f20ee2021efe58baa2c71228
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-19 00:35:49 -0500

    Update view todolist and profile to main

--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -13,7 +13,17 @@ struct MainView: View {
     var body: some View {
         if viewModel.isSignedIn, viewModel.currentUserId != "" {
             // signed in
-            ToDoListView()
+            TabView {
+                ToDoListView()
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+                
+                ProfileView()
+                    .tabItem {
+                        Label("Profile", systemImage: "person.circle")
+                    }
+            }
         } else {
             LoginView()
         }
