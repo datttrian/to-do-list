4c6dfbe3d2f4af3c19b8062ad28941bea8af156a
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-19 01:44:31 -0500

    Abstract accountView

--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -12,20 +12,25 @@ struct MainView: View {
     
     var body: some View {
         if viewModel.isSignedIn, viewModel.currentUserId != "" {
-            // signed in
-            TabView {
-                ToDoListView(userId: viewModel.currentUserId)
-                    .tabItem {
-                        Label("Home", systemImage: "house")
-                    }
-                
-                ProfileView()
-                    .tabItem {
-                        Label("Profile", systemImage: "person.circle")
-                    }
-            }
+            accountView
         } else {
             LoginView()
+        }
+    }
+    
+    @ViewBuilder
+    var accountView: some View {
+        // signed in
+        TabView {
+            ToDoListView(userId: viewModel.currentUserId)
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.circle")
+                }
         }
     }
 }
