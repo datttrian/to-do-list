c03031217be9e789497907f509d4e89d66b29b52
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-23 18:21:15 -0500

    Encapsulate fetched data into single item view

--- ToDoList/Views/ToDoListItemView.swift
+++ ToDoList/Views/ToDoListItemView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct ToDoListItemView: View {
+    let item: ToDoListItem
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.title)
+                    .font(.title)
+                
+                
+                Text("\(Date(timeIntervalSince1970: item.dueDate).formatted(date: .abbreviated, time: .shortened))")
+                    .font(.footnote)
+                    .foregroundColor(Color(.secondaryLabel))
+            }
+            
+            Spacer()
+            
+            Button{
+                
+            } label: {
+                Image(systemName:  item.isDone ? "checkmark.circle.fill" : "circle")
+            }
+        }
     }
 }
 
 #Preview {
-    ToDoListItemView()
+    ToDoListItemView(item: .init(id: "123",
+                                 title: "Test",
+                                 dueDate: Date().timeIntervalSince1970,
+                                 createdDate: Date().timeIntervalSince1970,
+                                 isDone: true))
 }
