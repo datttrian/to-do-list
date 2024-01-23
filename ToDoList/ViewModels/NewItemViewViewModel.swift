81e1a07b118ffc2eee8d2ddc90612dea2a6f9014
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-22 21:42:31 -0500

    Validate title inputs

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class NewItemViewViewModel: ObservableObject {
     @Published var title = ""
     @Published var dueDate = Date()
+    @Published var showAlert = false
     
     init () {}
     
