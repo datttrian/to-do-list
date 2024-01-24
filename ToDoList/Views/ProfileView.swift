5e6914edc89116bc75b461e45788c881c65f7a42
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-23 19:20:10 -0500

    Update avatar and info to profile view

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -13,57 +13,56 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                // Avatar
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(Color.blue)
-                    .frame(width: 125, height: 125)
-                    .padding()
-                
-                // Info: Name, Email, Member since
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("Name: ")
-                            .bold()
-                        Text("Afraz Siddiqui ")
+                if let user = viewModel.user {
+                    // Avatar
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .frame(width: 125, height: 125)
+                        .padding()
+                    
+                    // Info: Name, Email, Member since
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Name: ")
+                                .bold()
+                            Text(user.name)
+                        }
+                        .padding()
+                        
+                        HStack {
+                            Text("Email: ")
+                                .bold()
+                            Text(user.email)
+                        }
+                        .padding()
+                        
+                        HStack {
+                            Text("Member Since: ")
+                                .bold()
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                        }
+                        .padding()
                     }
                     .padding()
                     
-                    HStack {
-                        Text("Email: ")
-                            .bold()
-                        Text("Afraz Siddiqui ")
+                    // Sign out
+                    Button("Log Out") {
+                        viewModel.logOut()
                     }
+                    .tint(.red)
                     .padding()
                     
-                    HStack {
-                        Text("Member Since: ")
-                            .bold()
-                        Text("Afraz Siddiqui ")
-                    }
-                    .padding()
+                    Spacer()
+                } else {
+                    Text("Loading...")
                 }
-                .padding()
-                
-                // Sign out
-                Button("Log Out") {
-                    viewModel.logOut()
-                }
-                .tint(.red)
-                .padding()
-                
-                Spacer()
-                
             }
             .navigationTitle("Profile")
-            .toolbar {
-                Button {
-                    // Action
-                } label: {
-                    Image(systemName: "plus")
-                }
-            }
+        }
+        .onAppear {
+            viewModel.fetchUser()
         }
     }
 }
